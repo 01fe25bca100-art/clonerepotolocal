08448ac924432d972c3d3bd40a133032cc63c30f
01fe25bca100-art <01fe25bca100@kletech.ac.in>
2026-09-02 14:54:49 +0530

    Update collegeinfo.py

--- collegeinfo.py
+++ collegeinfo.py
@@ -1,6 +1,8 @@
 college = input("Enter your college name")
 city = input("enter your college city")
 contact = input("Enter your college contact number")
+location = input("Enter your location")
 
 print("college basic details",college " " +city)
 print("college contact is", contact)
+print("college location is", location)
